7c570e31f062d05d7f1d6b0f874a99b60ac2351c
Ethan Ma <em682@cornell.edu>
2020-08-05 19:39:44 -0400

    lolol

--- src/components/project.js
+++ src/components/project.js
@@ -16,8 +16,8 @@ export default function Project({id}) {
             <div> 
                 <div className="overlay">
                     <div className="icons">
-                        <DiJavascript1 color="#fff" size={'3vw'} />
-                        <DiReact color="#fff" size={'3vw'} />
+                        <DiJavascript1 color="#fff" size={'calc(20px + 3vw)'} />
+                        <DiReact color="#fff" size={'calc(20px + 3vw)'} />
                     </div>
                 </div>
                 <div className="projectImg">
@@ -29,10 +29,10 @@ export default function Project({id}) {
                     Features I worked on include an infinite scrolling feed, messaging, and filtering resources from the database.</p>
                     <div className="external">
                         <a href="https://github.com/umcody/runaway">
-                            <FiGithub color="#fff" size={'4vw'} />
+                            <FiGithub color="#fff" size={'calc(25px + 3vw)'} />
                         </a>
                         <a href="https://www.runawayapp.com">
-                            <FiExternalLink color="#fff" size={'4vw'} />
+                            <FiExternalLink color="#fff" size={'calc(25px + 3vw)'} />
                         </a>
                     </div>
                 </div>
@@ -42,7 +42,7 @@ export default function Project({id}) {
             <div>
                 <div className="overlay">
                 <div className="icons">
-                        <DiPython color="#fff" size={'3vw'} />
+                        <DiPython color="#fff" size={'calc(20px + 3vw)'} />
                     </div>
                 </div>
                 <div className="projectImg">
@@ -51,13 +51,13 @@ export default function Project({id}) {
                 <div className='descCard'>
                     <p className="title">Music Taste</p>
                     <p className="desc">Using pandas and Jupyter notebook, I performed data analysis on my listening 
-                    trends based on data from the Spotify web API. Data was then visualized with Infogram.</p>
+                    trends based on data from the Spotify Web API. Data was then visualized with Infogram.</p>
                     <div className="external">
                         <a href="https://github.com/em682/music-taste">
-                                <FiGithub color="#fff" size={'4vw'} />
+                                <FiGithub color="#fff" size={'calc(30px + 3vw)'} />
                         </a>
                         <a href="https://infogram.com/music-taste-1h8n6m1llv9m6xo?live">
-                                <FiExternalLink color="#fff" size={'4vw'} />
+                                <FiExternalLink color="#fff" size={'calc(25px + 3vw)'} />
                         </a>
                     </div>
                 </div>
@@ -66,9 +66,9 @@ export default function Project({id}) {
             <div>
                 <div className="overlay">
                 <div className="icons">
-                        <DiHtml5 color="#fff" size={'3vw'} />
-                        <DiCss3 color="#fff" size={'3vw'} />
-                        <DiPhp color="#fff" size={'3vw'} />
+                        <DiHtml5 color="#fff" size={'calc(20px + 3vw)'} />
+                        <DiCss3 color="#fff" size={'calc(20px + 3vw)'} />
+                        <DiPhp color="#fff" size={'calc(20px + 3vw)'} />
                     </div>
                 </div>
                 <div className="projectImg">
@@ -77,10 +77,10 @@ export default function Project({id}) {
                 <div className='descCard'>
                     <p className="title">Calisthenics</p>
                     <p className="desc">Created a basic website to practice backend development and database querying. 
-                    The site has a list of exercises with tags and descriptions, allowing users to upload their own pictures.</p>
+                    The site has a list of exercises with tags and descriptions, allowing user upload.</p>
                     <div className="external">
                         <a href="https://serene-forest-61580.herokuapp.com/index.php">
-                            <FiExternalLink color="#fff" size={'4vw'} />
+                            <FiExternalLink color="#fff" size={'calc(25px + 3vw)'} />
                         </a>
                     </div>
                 </div>

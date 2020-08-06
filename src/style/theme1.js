0d7475606e55ca5b50dc379c616b7e1a090fb858
Ethan Ma <em682@cornell.edu>
2020-08-06 14:55:20 -0400

    cleaned up css a bit

---
+++ src/style/theme1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const light ={
+    primary: '#ACDAFF',
+    secondary: '#E3F1FC',
+    tertiary: '#2E5F85',
+    foreground: "#000",
+    background: "#fff",
+    button: "#FF9EDA"
+}
